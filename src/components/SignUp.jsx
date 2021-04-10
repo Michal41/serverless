df84d4ce6dbe83e4ada7352d8937e1522482470a
michal <michal@michals-MacBook-Pro.local>
2021-04-10 17:09:57 +0200

    Code refactor

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,106 +1,87 @@
-
-
-import React from 'react';
-
+import React, { useState } from 'react';
 import {auth, createUserProfileDocument } from "../firebase/firebase.utils";
 
+const SignUp = () => {
+  const [displayName, setDisplayName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
-class SignUp extends React.Component{
-  constructor(){
-    super();
-    this.state = {
-      displayName:"",
-      email:"",
-      password:"",
-      confirmPassword:""
-    }
+  const handleChange = (event) => {
+    const onchange = eval(`set${event.target.name}`)
+    onchange(event.target.value)
   }
 
-  handleChange = (event) => {
-    this.setState({ [event.target.name] : event.target.value });
-  }
-
-  handleSubmit = async (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    const {displayName, email, password, confirmPassword }= this.state;
     if( password !== confirmPassword ){
       alert("password and confirmPasword not match");
       return;
     }
-
     try{
-
       const { user } = await auth.createUserWithEmailAndPassword(email,password)
       await createUserProfileDocument(user, {displayName});
-      this.setState({
-        displayName:"",
-        email:"",
-        password:"",
-        confirmPassword:""
-      });
-
+      setDisplayName('')
+      setEmail('')
+      setPassword('')
+      setConfirmPassword('')
     }catch (error){
       console.log(error);
     }
   }
 
-  render(){
-    const {displayName, email, password, confirmPassword }= this.state;
-    return(
-      <div className="sign-up">
-        <h2 className="title"> Sign Up </h2>
-        <span>Sign Up with your email and passoword </span>
+  return (
+    <div>
+    <h2> Sign Up </h2>
+    <span>Sign Up with your email and passoword </span>
 
-        <form className="sign-up-form" onSubmit={this.handleSubmit}>
+    <form onSubmit={handleSubmit}>
 
-          <input
-            type="text"
-            name="displayName"
-            value={displayName}
-            label="Display name"
-            onChange={this.handleChange}
-            placeholder="displayName"
-            required
-          />
+      <input
+        type="text"
+        name="DisplayName"
+        value={displayName}
+        label="Display name"
+        onChange={handleChange}
+        placeholder="displayName"
+        required
+      />
 
-          <input
-            type="email"
-            name="email"
-            value={email}
-            label="email"
-            placeholder="email"
-            onChange={this.handleChange}
-            required
-          />
+      <input
+        type="email"
+        name="Email"
+        value={email}
+        placeholder="email"
+        onChange={handleChange}
+        required
+      />
 
-          <input
-            type="password"
-            name="password"
-            value={password}
-            label="password"
-            placeholder="password"
-            onChange={this.handleChange}
-            required
-          />
-          <input
-            type="password"
-            name="confirmPassword"
-            value={confirmPassword}
-            label="confirm password"
-            placeholder="password"
-            onChange={this.handleChange}
-            required
-          />
-          <button
-            type="submit"
-          >
-            Create account
-          </button>
-        </form>
+      <input
+        type="password"
+        name="Password"
+        value={password}
+        placeholder="password"
+        onChange={handleChange}
+        required
+      />
+      <input
+        type="password"
+        name="ConfirmPassword"
+        value={confirmPassword}
+        placeholder="password"
+        onChange={handleChange}
+        required
+      />
+      <button
+        type="submit"
+      >
+        Create account
+      </button>
+    </form>
 
-      </div>
-    )
-  }
+  </div>
+  )
 }
+
 
 export default SignUp;
