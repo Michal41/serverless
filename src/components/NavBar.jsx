462619649d8666b3a42edd047d1a9953b9660ba1
michal <michal@michals-MacBook-Pro.local>
2021-04-14 21:39:10 +0200

    styles in navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,18 +5,26 @@ import {Link} from 'react-router-dom';
 const NavBar = (props) => {
   const { currentUser, logOut } = props;
   return(
-    <div>
-      {currentUser.id &&
-        <div>
-          <button onClick={logOut}> sign out</button>
-          <h1> logged user: {currentUser.displayName}</h1>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="/">Navigation</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <Link className="nav-item nav-link active" to='/'> Home</Link>
+          {!currentUser.id &&
+            <div>
+              <Link className="nav-item nav-link active" to='/sign-up'> sign-up</Link>
+              <Link className="nav-item nav-link active" to='/sign-in'> sign-in</Link>
+            </div>
+          }
+          {currentUser.id &&
+            <button className='btn btn-danger' onClick={logOut}> sign out</button>
+          }
         </div>
-      }
-    <Link to='/'> Home</Link>
-    <Link to='/sign-up'> sign-up</Link>
-    <Link to='/sign-in'> sign-in</Link>
-
-    </div>
+      </div>
+    </nav>
   )
 }
 
