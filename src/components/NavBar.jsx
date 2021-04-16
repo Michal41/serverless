0bf8aa91222a622d07891f185f49777386e90bfe
Jobson13 <50521404+Jobson13@users.noreply.github.com>
2021-04-16 18:23:48 +0200

    Gorącze mamuśki dla Sebastiana

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,14 +7,13 @@ const NavBar = (props) => {
   const classes = useStyles()
   const { currentUser, logOut } = props;
   return(
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className={`navbar navbar-expand-lg navbar-light bg-light ${classes.navBarContainer}`}>
     <Link to='/' className={classes.ask}>AskMe!</Link>
     <button className={"navbar-toggler"} type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div className={classes.NavBar} id="navbarNavAltMarkup">
       <div className="navbar-nav">
-        <Link className={"nav-item nav-link active"} to='/'> Home</Link>
         {!currentUser.id && <Link className="nav-item nav-link active" to='/sign-up'> Sign up</Link> }
         {!currentUser.id && <Link className="nav-item nav-link active" to='/sign-in'> Sign in</Link> }
         {currentUser.id && <button className='btn btn-danger' onClick={logOut}> sign out</button> }
@@ -25,8 +24,22 @@ const NavBar = (props) => {
 }
 
 const useStyles = createUseStyles({
+  navBarContainer: {
+    position: 'sticky',
+    width: '100%',
+    top: '0px',
+  },
   ask: {
     color: "#99a845",
+    marginLeft: 'auto',
+    marginRight: 'auto', 
+    fontSize: '20px'
+    
+  },
+  NavBar: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    fontSize: '20px'
   }
 })
 
